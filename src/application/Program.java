87bffaadc6ee963310fc0f822b9e7083471e2574
victor <vfinamore@gmail.com>
2019-01-30 15:08:00 -0200

    Dados date

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,7 +34,7 @@ public class Program {
 		System.out.print("How many contracts to this worker? ");
 		int n = sc.nextInt();
 
-		//informacoes e calculo da data
+		//informacoes
 		for (int i = 1; i <= n; i++) {
 			System.out.println("Enter contract #" + i + "data: ");
 			System.out.print("Date (DD/MM/YYYY): ");
